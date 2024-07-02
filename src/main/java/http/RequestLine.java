feaf82ea443a6cbfc4ce2640ff74f70eb0bc9f00
mirageoasis <kimhw0820@naver.com>
2024-07-02 13:08:48 +0900

    feat: 요청 첫번째 라인 받기

--- src/main/java/http/RequestLine.java
+++ src/main/java/http/RequestLine.java
@@ -1,17 +1,17 @@
 package http;
 
-public class StartLine {
+public class RequestLine {
     private final HttpMethod method;
     private final String path;
     private final String version;
 
-    public StartLine(HttpMethod method, String path, String version) {
+    public RequestLine(HttpMethod method, String path, String version) {
         this.method = method;
         this.path = path;
         this.version = version;
     }
 
-    public static StartLine fromRequest(String requestLine) {
+    public static RequestLine fromRequest(String requestLine) {
         String[] split = requestLine.split(" ");
         if(split.length != 3) {
             throw new IllegalArgumentException("Request Line 형식이 잘못되었습니다.");
@@ -20,7 +20,7 @@ public class StartLine {
         HttpMethod method = HttpMethod.of(split[0]);
         String path = split[1];
         String version = split[2];
-        return new StartLine(method, path, version);
+        return new RequestLine(method, path, version);
     }
 
     public HttpMethod getMethod() {
