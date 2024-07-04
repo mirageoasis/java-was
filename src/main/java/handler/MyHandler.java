83c64df362822eb4bf3741927cdfc81d3706ae58
mirageoasis <kimhw0820@naver.com>
2024-07-04 11:20:06 +0900

    fixed: body 수정 안되던 문제 해결

--- src/main/java/handler/MyHandler.java
+++ src/main/java/handler/MyHandler.java
@@ -4,17 +4,21 @@ import http.Http;
 import http.HttpMethod;
 import http.startline.RequestLine;
 import java.io.IOException;
+import util.LoggerUtil;
+import org.slf4j.Logger;
 
 abstract public class MyHandler {
-    //
+    private static final Logger logger = LoggerUtil.getLogger();
 
     public void handle(Http httpRequest, Http httpResponse) throws IOException {
         RequestLine requestLine = (RequestLine) httpRequest.getStartLine();
         HttpMethod httpMethod = requestLine.getMethod();
-
+        logger.info("Request: {}", httpRequest);
         if (httpMethod.equals(HttpMethod.GET)) {
+            logger.info("GET");
             doGet(httpRequest, httpResponse);
         } else if (httpMethod.equals(HttpMethod.POST)) {
+            logger.info("POST");
             doPost(httpRequest, httpResponse);
         }
     }
--- src/main/java/http/Http.java
+++ src/main/java/http/Http.java
@@ -3,7 +3,7 @@ package http;
 import http.startline.StartLine;
 
 public class Http {
-
+    // TODO: 인터페이스로 변경하는 것을 고려해볼 것
     private StartLine startLine;
     private final Header header;
     private byte[] body;
@@ -22,15 +22,15 @@ public class Http {
         return header;
     }
 
-    public void setBody(byte[] body) {
-        this.body = body;
-    }
-
     public void setStartLine(StartLine startLine) {
         this.startLine = startLine;
     }
 
     public byte[] getBody() {
         return body;
+    }
+
+    public void setBody(byte[] body) {
+        this.body = body;
     }
 }
